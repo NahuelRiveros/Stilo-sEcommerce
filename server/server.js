9ae8749c7e1083c2b96cd38f85dc266344ec9372
Riveros Edgardo Nahuel <70718987+NahuelRiveros@users.noreply.github.com>
2023-05-15 19:33:19 -0300

    Nueovs tambios Stripe implementado metodo de pago

--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
 import express  from "express";
 import cors from 'cors';
 import dotevn from 'dotenv';
+import stripe from 'stripe';
+import bodyParser from "body-parser";
+
 const env = dotevn.config()
 const app= express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT;
+const Stripe = stripe(process.env.STRIPE_SECRET_KEY)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+ app.post('/pagos',async(req,res)=>{
+    console.log(req.body.token)
+    await Stripe.charges.create({
+        source:req.body.token.id,
+        amount:req.body.amount,
+        currency:'usd',
+    });
+ });
+//     res.send('hello World!')
+// })
+// console.log('hello')
+
 app.listen(port,(req,res)=>{
     console.log(`sever esta corriendo en http://localhost:${port}/`)
 
