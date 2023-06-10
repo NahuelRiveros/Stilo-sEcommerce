9fac98822f8dc5ee638748fe9223d653f861adeb
Riveros Edgardo Nahuel <70718987+NahuelRiveros@users.noreply.github.com>
2023-06-10 15:15:28 -0300

    proba

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,24 @@
-import Sequelize from 'sequelize';
+import Sequelize from "sequelize";
 
-const db = new Sequelize ('Stilos', 'Pruebas', '123456789', {
-  host: 'localhost',
-  dialect:'postgres' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+const db = new Sequelize("Stilos", "postgres", "123456789", {
+    host: "localhost",
+    dialect:
+        "postgres" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
 });
-
 
 //Sincronizar base de datos
 
-db.sync({alter: true})
+async function createTables() {
+    try {
+        await db.sync({ force: true });
+        console.log("Tablas creadas en la base de datos.");
+    } catch (error) {
+        console.error("Error al crear las tablas:", error);
+    } finally {
+        db.close();
+    }
+}
+
+createTables();
 
 export default db;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,22 @@ import stripe from "stripe";
 import db from "./database/db.js";
 import bodyParser from "body-parser";
 
+import {
+    tbPersona,
+    tbUsuario,
+    tbCarrito,
+    tbCarritoDetalle,
+    tbCategoriaProd,
+    tbColorProd,
+    tbDescuentoProd,
+    tbDomicilio,
+    tbGeneroProd,
+    tbMarcaProd,
+    tbPermisosUsers,
+    tbProducto,
+    tbTalleProd,
+} from "./database/models.js";
+
 const env = dotevn.config();
 const app = express();
 app.use(cors());
